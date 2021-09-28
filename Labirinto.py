473d4491dcf03ab4f7946f05afeddd0779126709
Lucas Emmanuel <lucasemmanuelsa@gmail.com>
2021-09-28 12:51:12 -0300

    tela de rank adicionado

--- Labirinto.py
+++ Labirinto.py
@@ -1,14 +1,35 @@
-import time, pygame, os, sys
+import time, pygame, os, sys, sqlite3, banco_de_dados_gerador
 from random import randint
-
+from banco_de_dados_gerador import itens2
+from pygame.constants import MOUSEBUTTONDOWN, QUIT
 
 # inicializa todos os módulos que necessitam de inicialização dentro do pygame.
 pygame.init()
+
+#Criando conexão com o banco de dados e definindo o cursor:
+banco_de_dados = sqlite3.connect('nome_pontuacao.db')
+cursor = banco_de_dados.cursor() #Objeto que vai permitir realizar alterações no banco de dados
+
+
+#itens = cursor.fetchall() #Vai atribuir a variável 'itens' os 10 primeiros registros do banco de dados
+itens1 = itens2.copy()
+banco_de_dados.commit()
+
+
+#Condição para o código não quebrar quando ele for aberto pela primeira vez:
+if itens1 == []:
+    pass
 
 # Criar a janela
 janela = pygame.display.set_mode((500, 550))
 
 clock = pygame.time.Clock()
+
+#Variável que vai armazenar o nome do jogador temporariamente:
+nome = ""
+
+#Criar variável que vai armazenar a soma dos tempos de todos os mapas (Pontuação)
+pontos = 0
 
 diretorio_principal = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
@@ -33,7 +54,264 @@ verdelimao = (15, 255, 149)
 vermelho = (255, 49, 46)
 azul = (72, 172, 240)
 rosa = (252, 24, 152)
-roxo = (27, 20, 100)
+amarelo = (252,252,4)
+fonte = pygame.font.Font("Gamer.ttf",40)
+espessura = 9
+
+# Criar a janela
+largura_janela = 500
+altura_janela = 550
+
+#Criação dos botões Continuar e sair:
+class Botão:
+    def __init__(self,texto,largura_botão,alura_botão,posição,elevação):
+
+        #Atributos principais:
+        self.pressed = False
+        self.elevação = elevação
+        self.dynamic_elevação = elevação
+        self.original_y_posição = posição[1]
+
+        #Retângulo superior:
+        self.top_rect = pygame.Rect(posição,(largura_botão,alura_botão))
+        self.top_color = '#475F77'
+
+        #Segundo retângulo:
+        self.bottom_rect = pygame.Rect(posição,(largura_botão,elevação))
+        self.bottom_color = '#354B5E'
+
+        #texto do botão:
+        self.text_surf = fonte.render(texto,True,branco)
+        self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+
+    #Função para desenhar o botão:
+    def desenhar_botão(self):
+        #Efeito de botão:
+        self.top_rect.y = self.original_y_posição - self.dynamic_elevação
+        self.text_rect.center = self.top_rect.center
+
+        self.bottom_rect.midtop = self.top_rect.midtop
+        self.bottom_rect.height = self.top_rect.height + self.dynamic_elevação
+
+        pygame.draw.rect(janela,self.bottom_color,self.bottom_rect,border_radius = 12)
+        pygame.draw.rect(janela,self.top_color,self.top_rect,border_radius = 15)
+        janela.blit(self.text_surf,self.text_rect)
+        self.checar_click()
+    
+    #Função para verficar o click no botão:
+    def checar_click(self):
+        posição_mouse = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(posição_mouse):
+            self.top_color = vermelho
+        
+            if pygame.mouse.get_pressed()[0]:
+                self.dynamic_elevação = 0
+                self.pressed = True
+        
+            else:
+                self.dynamic_elevação = self.elevação
+                if self.pressed == True:
+                    self.pressed = False
+        else:
+            self.dynamic_elevação = self.elevação
+            self.top_color = azul
+
+#Função referente a tela de Ranking:
+def tela_rank(largura_janela,altura_janela):
+    janela_rank = pygame.display.set_mode((largura_janela, altura_janela))
+    fonte_ranking = pygame.font.Font('Gamer.ttf', 80)
+    fonte_ranking2 = pygame.font.Font('Gamer.ttf', 40)
+    while True:
+        mensagem_ranking = "HIGH SCORES"
+        texto_ranking = fonte_ranking.render(mensagem_ranking,True,(vermelho))
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()    
+
+            #Verifando se houve clique no botão Continuar:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x = pygame.mouse.get_pos()[0]
+                y = pygame.mouse.get_pos()[1]
+
+                #Verificando se houve clique no botão Sair:
+                if x > 170 and y > 510 and x < 330 and y < 540:
+                    pygame.quit()
+                    exit()
+
+                """ if x > 30 and y > 510 and x < 190 and y < 540:
+                    pass """ #Rever
+
+                
+
+        #Inserindo texto de rank, score e nome:
+        mensagem_rank = "RANK"
+        texto_rank = fonte_ranking2.render(mensagem_rank,True,(amarelo))
+
+        mensagem_score = "SCORE"
+        texto_score = fonte_ranking2.render(mensagem_score,True,(amarelo))
+
+        mensagem_name = "NAME"
+        texto_name = fonte_ranking2.render(mensagem_name,True,(amarelo))
+
+        #Textos das posições:
+        mensagem_posição1 = "1ST"
+        texto_posição1 = fonte_ranking2.render(mensagem_posição1,True,(branco))
+
+        mensagem_posição2 = "2ND"
+        texto_posição2 = fonte_ranking2.render(mensagem_posição2,True,(vermelho))
+
+        mensagem_posição3 = "3RD"
+        texto_posição3 = fonte_ranking2.render(mensagem_posição3,True,(amarelo))
+
+        mensagem_posição4 = "4TH"
+        texto_posição4 = fonte_ranking2.render(mensagem_posição4,True,(amarelo))
+
+        mensagem_posição5 = "5TH"
+        texto_posição5 = fonte_ranking2.render(mensagem_posição5,True,(verdelimao))
+
+        mensagem_posição6 = "6TH"
+        texto_posição6 = fonte_ranking2.render(mensagem_posição6,True,(azul))
+
+        mensagem_posição7 = "7TH"
+        texto_posição7 = fonte_ranking2.render(mensagem_posição7,True,(azul))
+
+        mensagem_posição8 = "8TH"
+        texto_posição8 = fonte_ranking2.render(mensagem_posição8,True,(vermelho))
+
+        mensagem_posição9 = "9TH"
+        texto_posição9 = fonte_ranking2.render(mensagem_posição9,True,(branco))
+
+        mensagem_posição10 = "10TH"
+        texto_posição10 = fonte_ranking2.render(mensagem_posição10,True,(rosa))
+
+        #Textos dos scores:
+        mensagem_score1 = str(itens1[0][1])
+        texto_score1 = fonte_ranking2.render(mensagem_score1,True,(branco))
+
+        
+        mensagem_score2 = str(itens1[1][1])
+        texto_score2 = fonte_ranking2.render(mensagem_score2,True,(vermelho))
+
+        
+        mensagem_score3 = str(itens1[2][1])
+        texto_score3 = fonte_ranking2.render(mensagem_score3,True,(amarelo))
+
+        
+        
+        mensagem_score4 = str(itens1[3][1])
+        texto_score4 = fonte_ranking2.render(mensagem_score4,True,(amarelo))
+
+        
+        mensagem_score5 = str(itens1[4][1])
+        texto_score5 = fonte_ranking2.render(mensagem_score5,True,(verdelimao))
+
+        
+        mensagem_score6 = str(itens1[5][1])
+        texto_score6 = fonte_ranking2.render(mensagem_score6,True,(azul))
+
+        
+        mensagem_score7 = str(itens1[6][1])
+        texto_score7 = fonte_ranking2.render(mensagem_score7,True,(azul))
+
+        
+        mensagem_score8 = str(itens1[7][1])
+        texto_score8 = fonte_ranking2.render(mensagem_score8,True,(vermelho))
+
+        
+        mensagem_score9 = str(itens1[8][1])
+        texto_score9 = fonte_ranking2.render(mensagem_score9,True,(branco))
+        
+        
+        mensagem_score10 = str(itens1[9][1])
+        texto_score10 = fonte_ranking2.render(mensagem_score10,True,(rosa))
+
+        #Textos dos nomes:
+        mensagem_nome1 = itens1[0][2]
+        texto_nome1 = fonte_ranking2.render(mensagem_nome1,True,(branco))
+
+        mensagem_nome2 = itens1[1][2]
+        texto_nome2 = fonte_ranking2.render(mensagem_nome2,True,(vermelho))
+
+        mensagem_nome3 = itens1[2][2]
+        texto_nome3 = fonte_ranking2.render(mensagem_nome3,True,(amarelo))
+
+        mensagem_nome4 = itens1[3][2]
+        texto_nome4 = fonte_ranking2.render(mensagem_nome4,True,(amarelo))
+
+        mensagem_nome5 = itens1[4][2]
+        texto_nome5 = fonte_ranking2.render(mensagem_nome5,True,(verdelimao))
+
+        mensagem_nome6 = itens1[5][2]
+        texto_nome6 = fonte_ranking2.render(mensagem_nome6,True,(azul))
+
+        mensagem_nome7 = itens1[6][2]
+        texto_nome7 = fonte_ranking2.render(mensagem_nome7,True,(azul))
+
+        mensagem_nome8 = itens1[7][2]
+        texto_nome8 = fonte_ranking2.render(mensagem_nome8,True,(vermelho))
+
+        mensagem_nome9 = itens1[8][2]
+        texto_nome9 = fonte_ranking2.render(mensagem_nome9,True,(branco))
+
+        mensagem_nome10 = itens1[9][2]
+        texto_nome10 = fonte_ranking2.render(mensagem_nome10,True,(rosa))
+
+
+        #botão_continuar.desenhar_botão() REVER
+        botão_sair.desenhar_botão()
+
+        #Desenhar tabelas do rank:
+        janela_rank.blit(texto_rank,(70,100))
+        janela_rank.blit(texto_score,(210,100))
+        janela_rank.blit(texto_name,(350,100))
+        janela_rank.blit(texto_ranking,(90,0))
+
+        #Desenhar números referentes a cada posição:
+        janela_rank.blit(texto_posição1,(75,140))
+        janela_rank.blit(texto_posição2,(75,170))
+        janela_rank.blit(texto_posição3,(75,200))
+        janela_rank.blit(texto_posição4,(75,230))
+        janela_rank.blit(texto_posição5,(75,260))
+        janela_rank.blit(texto_posição6,(75,290))
+        janela_rank.blit(texto_posição7,(75,320))
+        janela_rank.blit(texto_posição8,(75,350))
+        janela_rank.blit(texto_posição9,(75,380))
+        janela_rank.blit(texto_posição10,(75,410))
+
+        #Desenhar scores:
+        janela_rank.blit(texto_score1,(240,140))
+        janela_rank.blit(texto_score2,(240,170))
+        janela_rank.blit(texto_score3,(240,200))
+        janela_rank.blit(texto_score4,(240,230))
+        janela_rank.blit(texto_score5,(240,260))
+        janela_rank.blit(texto_score6,(240,290))
+        janela_rank.blit(texto_score7,(240,320))
+        janela_rank.blit(texto_score8,(240,350))
+        janela_rank.blit(texto_score9,(240,380))
+        janela_rank.blit(texto_score10,(240,410))
+
+        #Desenhar nomes:
+        janela_rank.blit(texto_nome1,(360,140))
+        janela_rank.blit(texto_nome2,(360,170))
+        janela_rank.blit(texto_nome3,(360,200))
+        janela_rank.blit(texto_nome4,(360,230))
+        janela_rank.blit(texto_nome5,(360,260))
+        janela_rank.blit(texto_nome6,(360,290))
+        janela_rank.blit(texto_nome7,(360,320))
+        janela_rank.blit(texto_nome8,(360,350))
+        janela_rank.blit(texto_nome9,(360,380))
+        janela_rank.blit(texto_nome10,(360,410))
+        
+        pygame.display.flip() # atualiza a tela 
+        clock.tick(60)
+    
+    return janela_rank
+
+#Criandos objetos derivados da classe Botão:
+#botão_continuar = Botão("Continuar",160,30,(30,510),6) REVER!
+botão_sair = Botão("Sair",160,30,(170,510),6)  
 
 def criarCanhão(imagem ,x, y):
     janela.blit(imagem, (x, y))
@@ -169,8 +447,8 @@ jogadorImagem = pygame.transform.scale(jogadorImagem, (14, 14))
 rectJogador = jogadorImagem.get_rect()
 
 # Posiçao do jogador
-jogadorX = 25 #438
-jogadorY = 25 #27
+jogadorX = 25 #438 #25 (Padrão)
+jogadorY = 25 #27 #25 (Padrão)
 jogadorMudançaX = 0
 jogadorMudançaY = 0
 # Velocidade do jogador
@@ -191,6 +469,7 @@ def menu (janela,wallpaper):
     fonte_base      = pygame.font.SysFont("Arial",110)
     strJogador     = ''
     ContadorLetras  = 4
+    global nome
 
     def wallpaper_changer (num_wallpaper):
         fundo = pygame.image.load(os.path.join(diretorio_imagens, 'wallpaper{}.png'.format(num_wallpaper)))
@@ -218,8 +497,14 @@ def menu (janela,wallpaper):
                     strJogador = strJogador.upper()
 
                 elif event.key == pygame.K_SPACE and len(strJogador) == 3:
-                    return strJogador
+                    nome = strJogador #Inserindo o nome do jogador na variável nome
+                    return strJogador 
+                    
+                    
 
+                    
+                    
+        
         text_surface = fonte_base.render(strJogador,True,(255,255,255))
         comando = pygame.key.get_pressed()
 
@@ -232,6 +517,7 @@ def menu (janela,wallpaper):
 
         pygame.display.update()
 
+
 menu(janela,0)
 #-----------------------------------------------------------------------------------------
 janela.fill(preto)  # pinta a cor do fundo
@@ -242,7 +528,7 @@ texto = fonte.render('Nível 1', True, vermelho)
 janela.blit(texto, (180, 215))
 
 pygame.display.flip()
-time.sleep(3)
+time.sleep(3) 
 #-----------------------------------------------------------------------------------------------------
 listaX = []
 listaY = []
@@ -289,6 +575,7 @@ sair = False
 while sair != True: #CÓDIGO REFERENTE AO MAPA 1
     
     tempoMapa1 = int(pygame.time.get_ticks()/1000) - 3 # contagem do tempo
+    
 
     janela.fill(preto) # pinta a janela de preto novamente
 
@@ -404,16 +691,18 @@ while sair != True: #CÓDIGO REFERENTE AO MAPA 1
         jogadorX = 462
 
     if jogador.colliderect(chegada):
+        pontos = tempoMapa1
+        
         #-----------------------------------------------------
         # Tela antes do próximo mapa
         janela.fill(preto)
         texto2 = fonte.render('Nível 2', True, vermelho)
         janela.blit(texto2, (180, 215))
         pygame.display.flip()
-        time.sleep(5)
+        time.sleep(3) 
         #-----------------------------------------------------
-        jogadorX = 25
-        jogadorY = 60 #29 para pular o mapa
+        jogadorX = 25 #25 (Padrão)
+        jogadorY = 60 #29 para pular o mapa 60 (Padrão)
 
         chegadaX = 25
         chegadaY = 29
@@ -467,6 +756,7 @@ while sair != True: #CÓDIGO REFERENTE AO MAPA 1
 
             tempoMapa2 = int(pygame.time.get_ticks()/1000) - (tempoMapa1 + 9)
             cronometro = fonte.render('Tempo: ' + str(tempoMapa2), True, branco)
+            
 
             janela.blit(cronometro, (285, 490))
 
@@ -583,16 +873,17 @@ while sair != True: #CÓDIGO REFERENTE AO MAPA 1
                 sair = True
 
             if jogador.colliderect(chegada): #passa de mapa
+                pontos += tempoMapa2 # tempoMapa1 + tempoMapa2
                 #-----------------------------------------------------
                 # Tela antes do próximo mapa
                 janela.fill(preto)
                 texto3 = fonte.render('Nível 3', True, vermelho)
                 janela.blit(texto3, (180, 215))
                 pygame.display.flip()
-                time.sleep(5)
+                time.sleep(3) 
                 #-----------------------------------------------------
-                jogadorX = 462
-                jogadorY = 25
+                jogadorX = 462 #462 (Padrão)
+                jogadorY = 25 #25 (Padrão)
 
                 chegadaX = 462
                 chegadaY = 462
@@ -814,8 +1105,17 @@ while sair != True: #CÓDIGO REFERENTE AO MAPA 1
                     if jogador.colliderect(chegada):
                         chegadaX = 25
                         chegadaY = 25
-                        pygame.display.flip()
-                        mudouChegada = True
+                        pontos += tempoBala3 #tempoMapa1 + tempoMapa2 + tempoMapa3
+
+                        #Inserindo pontuação e nome no banco de dados:
+                        cursor.execute("INSERT INTO nomes_pontos VALUES("+str(pontos)+",'"+nome+"')")
+                        cursor.execute("SELECT rowid, * FROM nomes_pontos ORDER BY Score LIMIT 10")
+                        banco_de_dados.commit() #Aplicando alterações no banco de dados.
+                        print(itens1)
+                        banco_de_dados.close()
+                        tela_rank(largura_janela,altura_janela)
+                        mudouChegada = True 
+                        
                     if chegadaX == 25 and chegadaY == 25 and jogador.colliderect(pygame.Rect(25,25, 12,12)):
                         sair = True
 
@@ -827,4 +1127,3 @@ while sair != True: #CÓDIGO REFERENTE AO MAPA 1
 
     pygame.display.flip() # atualiza a tela 
     clock.tick(60)
-
